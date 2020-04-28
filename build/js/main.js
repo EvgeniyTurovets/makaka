dd1b73fbbb6b9d0b58b24c2ff52beb9db48cd72d
sanyokstd <59456430+sanyokstd@users.noreply.github.com>
2020-04-28 22:46:38 +0300

    1
    1

--- build/js/main.js
+++ build/js/main.js
@@ -3205,12 +3205,17 @@ $(function(){
         $('#app').append( $('.composer-panel') );
       }
      
-      
-      $('.composer-panel') .on('touchstart',function(e){
-        $('#viewport').css('overflow', 'hidden')
-        $('html').css('overflow', 'hidden')
-      });
-      $('.composer-panel') .on('touchend',function(e){
-        $('#viewport').css('overflow', 'auto')
+     
+
+      // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+      let vh = window.innerHeight * 0.01;
+      // Then we set the value in the --vh custom property to the root of the document
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+      // We listen to the resize event
+      window.addEventListener('resize', () => {
+        // We execute the same script as before
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
       });
 })
