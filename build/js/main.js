f81989e2b0eff3e3487eadccdd0beb41d0c25b0b
sanyokstd <59456430+sanyokstd@users.noreply.github.com>
2020-04-29 16:35:41 +0300

    1
    1

--- build/js/main.js
+++ build/js/main.js
@@ -3202,34 +3202,62 @@ $(function(){
        }
       })
       if($(window).width() < 576){
-      //   $('body').append( $('.composer-panel') );
+     
+     
+      function bodyFixPosition() {
+
+        setTimeout( function() {
+        /* Ставим необходимую задержку, чтобы не было «конфликта» в случае, если функция фиксации вызывается сразу после расфиксации (расфиксация отменяет действия расфиксации из-за одновременного действия) */
       
-
-      //   let bodyH = $('body').height()
+          if ( !document.body.hasAttribute('data-body-scroll-fix') ) {
       
-      //   let htmlH = $('html').height()
-        
-      //   $('#app').scroll(function() { 
-      //     $('body').css('height', bodyH)
-      //     $('body').css('overflow', 'hidden')
-      //     $('html').css('height', htmlH)
-      //     $('body').css('overflow', 'hidden')
-
-      //     $('.composer-panel').css('height', '87px')
-      //     $('.composer-panel').css('bottom', '0')
-      //   });
-      //   $(document).on('touchstart', function(e){
-      //     $('body').css('height', bodyH)
-      //     $('body').css('overflow', 'hidden')
-      //     $('html').css('height', htmlH)
-      //     $('body').css('overflow', 'hidden')
-
-      //     $('.composer-panel').css('height', '87px')
-      //     $('.composer-panel').css('bottom', '0')
-      // });
-      $(document).on('touchmove',function(e){
-        e.preventDefault();
-      });
+            // Получаем позицию прокрутки
+            let scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+      
+            // Ставим нужные стили
+            document.body.setAttribute('data-body-scroll-fix', scrollPosition); // Cтавим атрибут со значением прокрутки
+            document.body.style.overflow = 'hidden';
+            document.body.style.position = 'fixed';
+            document.body.style.top = '-' + scrollPosition + 'px';
+            document.body.style.left = '0';
+            document.body.style.width = '100%';
+      
+          }
+      
+        }, 15 ); /* Можно задержку ещё меньше, но у меня работало хорошо именно с этим значением на всех устройствах и браузерах */
+      
+      }
+      
+      // 2. Расфиксация <body>
+      function bodyUnfixPosition() {
+      
+        if ( document.body.hasAttribute('data-body-scroll-fix') ) {
+      
+          // Получаем позицию прокрутки из атрибута
+          let scrollPosition = document.body.getAttribute('data-body-scroll-fix');
+      
+          // Удаляем атрибут
+          document.body.removeAttribute('data-body-scroll-fix');
+      
+          // Удаляем ненужные стили
+          document.body.style.overflow = '';
+          document.body.style.position = '';
+          document.body.style.top = '';
+          document.body.style.left = '';
+          document.body.style.width = '';
+      
+          // Прокручиваем страницу на полученное из атрибута значение
+          window.scroll(0, scrollPosition);
+      
+        }
+      
+      }
+      $('.composer-panel').find('textarea').focusin(function(){
+        bodyFixPosition()
+      })
+      $('.composer-panel').find('textarea').focusout(function(){
+        bodyUnfixPosition()
+      })
     }
     
 })
